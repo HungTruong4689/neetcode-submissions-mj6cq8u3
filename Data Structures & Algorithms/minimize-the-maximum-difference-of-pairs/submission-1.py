01f874963e04f5a0b692e6710c1376ed5a8b19b4
HungTruong4689 <101930463+HungTruong4689@users.noreply.github.com>
2026-05-02 09:04:41 +0700

    Add: minimize-the-maximum-difference-of-pairs - submission-1

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        nums.sort()
+        l = []
+        for i in range(1,len(nums),2):
+            val = nums[i] - nums[i-1]
+            if val not in l:
+                l.append(val)
+        #l.sort()
+        print(l)
+        return max(l[:p])
